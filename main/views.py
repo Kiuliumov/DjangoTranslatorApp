0c703947c4c3a9a897cd26bedcee63c82a2384b4
Kiuliumov <miti.bania@gmail.com>
2024-04-28 19:32:42 +0300

    commit

--- main/views.py
+++ main/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, HttpResponse
+from translate import Translator
 
 # Create your views here.
+
+from django.shortcuts import render
+from translate import Translator
+
+
+from django.shortcuts import render
+from translate import Translator
+
+def home(request):
+    if request.method == "POST":
+
+        text = request.POST.get("translate", "")
+        language = request.POST.get("language", "")
+        if text:
+            translator = Translator(to_lang=language)
+            translation = translator.translate(text)
+            return render(request, "main/translation_result.html", {"translation": translation})
+
+    return render(request, "main/index.html")
